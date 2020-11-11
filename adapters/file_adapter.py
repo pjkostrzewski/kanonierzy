87fc887b8134746c5e19b807f2cafdfb6287249a
Patryk Kostrzewski <pjkostrzewski@gmail.com>
2020-11-11 16:05:31 +0100

    encoding, ensure_ascii

--- adapters/file_adapter.py
+++ adapters/file_adapter.py
@@ -9,15 +9,15 @@ class FileAdapter:
     @staticmethod
     def datetime_converter(something):
         if isinstance(something, datetime.datetime):
-            return something.__str__()
+            return something.timestamp()
 
     def save_to_file(self, data):
-        with open(self.filename, 'w') as outfile:
-            json.dump(data, outfile, default=self.datetime_converter)
+        with open(self.filename, 'w', encoding="utf-8") as outfile:
+            json.dump(data, outfile, default=self.datetime_converter, ensure_ascii=False)
 
     def append_to_file(self, data):
-        with open(self.filename, 'a') as outfile:
-            json.dump(data, outfile)
+        with open(self.filename, 'a', encoding="utf-8") as outfile:
+            json.dump(data, outfile, default=self.datetime_converter, ensure_ascii=False)
 
     def read_file(self):
         with open(self.filename, 'r') as json_file:
